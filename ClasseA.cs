8ebd719261f89780745c70f160c1523e30ab7af8
salvatore <salvatore.sorrentino@live.com>
2023-05-30 17:56:11 +0200

    Aggiungere i file di progetto.

---
+++ ClasseA.cs
@@ -0,0 +1,76 @@
+﻿namespace DemoDependency;
+
+public  class ClasseA
+{
+    private readonly IClasseB classeB;
+
+    public ClasseA(IClasseB classeB)
+    {
+        this.classeB = classeB;
+    }
+    public void FaiQualcosaA()
+    {
+        //var classeb = new ClasseB();
+        var tempo = DateTime.Now;
+
+        Console.WriteLine("Fai Qualcosa A " + tempo.ToLongTimeString());
+        classeB.FaiQualcosaB();
+    }
+}
+
+
+public interface IClasseB
+{
+    void FaiQualcosaB();
+}
+
+public class ClasseC : IClasseB
+{
+    public void FaiQualcosaB()
+    {
+        Console.WriteLine("Classe c"); ;
+    }
+}
+
+
+
+public class ClasseB : IClasseB
+{
+    private readonly int parameter;
+
+    public ClasseB(int parameter)
+    {
+        this.parameter = parameter;
+    }
+
+
+    public void FaiQualcosaB()
+    {
+        if (parameter < 15)
+        {
+            throw new Exception();
+        }
+        Console.WriteLine("Fai Qualcosa B");
+    }
+
+}
+
+//public class ClasseB
+//{
+//    private readonly int parameter;
+
+//    public ClasseB(int parameter)
+//    {
+//        this.parameter = parameter;
+//    }
+
+
+//    public void FaiQualcosaB()
+//    {
+//        if(parameter < 15)
+//        {
+//            throw new Exception();
+//        }
+//        Console.WriteLine("Fai Qualcosa B");
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DemoDependency;
+
+Console.WriteLine("Hello, World!");
+
+// var b = new ClasseB(8);
+IClasseB b = new ClasseB(8);
+IClasseB c = new ClasseC();
+
+var a = new ClasseA(c);
+
+a.FaiQualcosaA();
